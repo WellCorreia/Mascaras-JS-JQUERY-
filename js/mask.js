0a87fd75af52fa13416556edbb5605e68f2ebe5d
Wellington <wellington.correia02@gmail.com>
2016-12-17 00:09:01 -0300

    Arquivos contendo todo o código necessário para a geração de mascaras com JQuery.

---
+++ js/mask.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("input#data").mask("99/99/9999");
+    $("input#cpf").mask("999.999.999-99");
+    $("input#cep").mask("99.999-999");
+    $("input#celular").mask("(99) 99999-9999");
+    $("input#telefone").mask("(99) 9999-9999");
+});
